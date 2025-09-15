faebdbc7e49e4ca19903b54ec5922b9caa315d00
Wendyz-811 <yz11326@nyu.edu>
2025-09-15 12:48:17 -0400

    update

--- recipe-card/src/index.js
+++ recipe-card/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// CORE REACT LIBRARY
+import React from 'react'
+// REACT DOM is for browser based projects
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//import our App component so we can render it
+import App from './App'
+
+// import global style sheet for fonts and resets
+import './global.css'
+
+// grab a element by ID to assign as the root entry for our react project
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
